4bb5abe92d4c85840c23ba0a12623b61dfb648b6
SDKInsomnia <71098820+SDKInsomnia@users.noreply.github.com>
2020-12-01 18:27:47 +0300

    Update botik.py

--- botik.py
+++ botik.py
@@ -6,6 +6,7 @@ from discord import Activity, ActivityType
 prefix="-"
 
 client=commands.Bot(command_prefix=prefix)
+client.remove_command('help')
 
 @client.event
 async def on_ready():
@@ -21,6 +22,16 @@ async def clear(ctx,amount:int=None):
     else:
         await ctx.message.delete()
         await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def help(ctx):
+    emb=discord.Embed(title='Список команд Izya:',description=f'**-clear - очистить чат\n-help - информация о командах**', color=0xf47fff, timestamp=ctx.message.created_at)
+    emb.set_footer(text=f'{ctx.author.name}', icon_url=ctx.author.avatar_url)
+    emb.set_author(name=f'{ctx.author.name}', icon_url=ctx.author.avatar_url)
+    emb.add_field(name='cooper',value="ya123",inline=False)
+    emb.set_image(url=ctx.author.avatar_url)
+    emb.set_thumbnail(url=ctx.author.avatar_url)
+    await ctx.send(embed=emb)
         
 token=os.environ.get('TOKEN')
 client.run(str(token))
