74f427bace7ba8bff27bf95ea8eeceed7c50f694
SDKInsomnia <71098820+SDKInsomnia@users.noreply.github.com>
2020-12-01 14:38:53 +0300

    Update botik.py

--- botik.py
+++ botik.py
@@ -3,39 +3,24 @@ import os
 from discord.ext import commands
 from discord import Activity, ActivityType
 
-client=commands.Bot(command_prefix='-')
+prefix="-"
+
+client=commands.Bot(command_prefix=prefix)
 
 @client.event
 async def on_ready():
-    print( 'Izya подключен' )
+    print("Бот подключен")
     await client.change_presence(status = discord.Status.online, activity = Activity(name = 'за Arizona Squad', type = ActivityType.watching))
 
 @client.command()
-async def chelp(ctx):
-    emb=discord.Embed(title='opisanie',description=f'**Privet, {ctx.author.display_name}**', color=0xf47fff, timestamp=ctx.message.created_at)
-    emb.set_footer(text=f'{ctx.author.name}', icon_url=ctx.author.avatar_url)
-    emb.set_author(name=f'{ctx.author.name}', icon_url=ctx.author.avatar_url)
-    emb.add_field(name='cooper',value="ya123",inline=False)
-    emb.set_image(url=ctx.author.avatar_url)
-    emb.set_thumbnail(url=ctx.author.avatar_url)
-    await ctx.send(embed=emb)
-
-@client.command()
-async def magic(ctx):
-    e=discord.Embed()
-    e.set_author(name=f'{ctx.author.name}', icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=e)
-
-@client.command()
-async def cc(ctx,amount:int):
-    await ctx.message.delete()
-    await ctx.channel.purge(limit=amount)
-
-@client.event
-async def on_message(message):
-    if message.author.premium_since:
-        await message.add_reaction('<:booster:750658847634358383>')
-    await client.process_commands(message)
-
+async def clear(ctx,amount:int=None):
+    if amount is None:
+        await ctx.send("**Необходимо ввести количество сообщений. Пример: -clear 10**")
+    elif amount > 20:
+        await ctx.send("**Нельзя очистить больше 20-ти сообщений за раз.**")
+    else:
+        await ctx.message.delete()
+        await ctx.channel.purge(limit=amount)
+        
 token=os.environ.get('TOKEN')
 client.run(str(token))
