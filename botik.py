c9826ac02bd0987bce9ebc7efed49ae9b4282527
SDKInsomnia <71098820+SDKInsomnia@users.noreply.github.com>
2020-12-02 12:26:25 +0300

    Update botik.py

--- botik.py
+++ botik.py
@@ -3,6 +3,7 @@ import os
 from discord.ext import commands
 from discord import Activity, ActivityType
 from discord.utils import get
+from discord import Member
 
 prefix="-"
 
@@ -12,7 +13,12 @@ client.remove_command('help')
 @client.event
 async def on_ready():
     print("Бот подключен")
-    await client.change_presence(status = discord.Status.online, activity = Activity(name = 'за AS & ERP', type = ActivityType.watching))
+    await client.change_presence(status = discord.Status.online, activity = Activity(name = 'за Arizona Squad', type = ActivityType.watching))
+
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.guild.roles, id = 692309041564745728)
+    await member.add_roles(role)
 
 @client.command()
 async def clear(ctx,amount:int=None):
@@ -28,34 +34,6 @@ async def clear(ctx,amount:int=None):
 async def help(ctx):
     emb=discord.Embed(title='Список команд бота "Izya":',description=f'**-help - информация о командах.\n-clear - очистить сообщения в чате.**', color=0xf47fff, timestamp=ctx.message.created_at)
     await ctx.send(embed=emb)
-
-@client.command()
-async def join(ctx):
-    global voice
-    channel = ctx.message.author.voice.channel
-    voice = get(client.voice_clients, guild = ctx.guild)
-
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-        await ctx.send(f'Izya присоединился к каналу.')
-
-@client.command()
-async def leave(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = get(client.voice_clients, guild = ctx.guild)
-
-    if voice and voice.is_connected():
-        await voice.disconnect()
-    else:
-        voice = await channel.connect()
-        await ctx.send(f'Izya отключился от канала.')
-
-@client.event
-async def on_member_join(member):
-  role = client.get_role(692309041564745728)
-  await member.add_roles(role)
 
 token=os.environ.get('TOKEN')
 client.run(str(token))
